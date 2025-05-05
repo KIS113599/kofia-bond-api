96380aab2117c2e472c98bcee12f514938641b3d
KIS113599 <seojin113599@gmail.com>
2025-05-06 00:05:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import requests
 from datetime import datetime, timedelta
+import os
 
 app = Flask(__name__)
 
@@ -53,4 +54,5 @@ def get_korea_bond_yields():
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
